f69ed4345e0a8826a30762903d05a3b897e9b8c7
Magerram Zeynalov <zeinalov@doczilla.pro>
2022-01-10 01:14:54 +0300

    template

---
+++ src/main/java/com/vgtstptlk/simaproject/SimaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.vgtstptlk.simaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimaProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vgtstptlk/simaproject/SimaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vgtstptlk.simaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
